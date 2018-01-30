2d3a4353d982c6e38f45e19fba82f540a4e569b7
Brandon <bcarvalho2500@gmail.com>
2018-01-30 11:02:40 -0500

    Adds the statement to compare the numbers to largest and smallest and adds a description of the program.

--- TwoDarrays2.java
+++ TwoDarrays2.java
@@ -1,3 +1,7 @@
+/*
+    A program using a 2D array and finds the largest and smallest
+    number of all the numbers in the list.
+*/
 public class TwoDarrays2
 {
     public static void main (String args[])
@@ -9,6 +13,19 @@ public class TwoDarrays2
         int largest = a[0][0];
         int smallest = a[0][0];
 
-        for (int i = 0;)
+        for (int i = 0; i < a.length; i++)
+        {
+            for (int j = 0; j < a[0].length; j++)
+            {
+                if (a[i][j] > largest)
+                {
+                    largest = a[i][j];
+                }
+                if (a[i][j] < smallest)
+                {
+                    smallest = a[i][j];
+                }
+            }
+        }
     }
 }
