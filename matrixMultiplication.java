1ac07a66e43a9356143c8773c8007ccce6dbe18d
Brandon <bcarvalho2500@gmail.com>
2018-02-04 12:19:28 -0500

    Creates the file

---
+++ matrixMultiplication.java
@@ -0,0 +1,31 @@
+public class matrixMultiplication
+{
+    public static void main (String args[])
+    {
+
+    }
+}
+
+class matrix
+{
+    private int[][] matrix1;
+    private int[][] matrix2;
+
+    public matrix()
+    {
+        matrix1 = {{1,2,-2,0},
+                   {-3,4,7,2},
+                   {6,0,3,1}};
+        matrix2 = {{-1,3},
+                   {0,9},
+                   {1,-11},
+                   {4,-5}};
+    }
+
+    public int[][] multiplyMatrices ()
+    {
+        int[][] productMatrix = [matrix1.length][matix2[0].length];
+
+        for (int )
+    }
+}
